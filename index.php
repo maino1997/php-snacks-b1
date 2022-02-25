40fde7640bd51048dd32d6e9c0d182d50ae26397
Sasha Mainardi <sashamainardi4@gmail.com>
2022-02-25 17:46:27 +0100

    snack 2 completed

--- index.php
+++ index.php
@@ -22,6 +22,57 @@ if(strlen($name) < 3 && strpos($mail , '.') && strpos($mail , '@') && is_numeric
 }
 ?> 
 
+<!-- Snack 2
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 
+01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. -->
+
+<?php 
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+$keys = array_keys((array)$posts);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -34,5 +85,34 @@ if(strlen($name) < 3 && strpos($mail , '.') && strpos($mail , '@') && is_numeric
     <h2>
 <?php echo $message ?>
     </h2>
-</body>
+
+    <h2>POSTS DATES</h2>
+    <ul>
+        <?php for($i = 0; $i < count($keys); $i++) : ?>
+
+            <li> <?php echo $keys[$i]   ?> </li>
+
+            
+            <?php for($y = 0; $y < count($posts[$keys[$i]]); $y++) : ?>            
+                
+             <ul>
+
+                 <li> <?php echo $posts[$keys[$i]][$y]['title']  ?> </li>
+                 <li> <?php echo $posts[$keys[$i]][$y]['author']  ?> </li> 
+                 <li> <?php echo $posts[$keys[$i]][$y]['text']   ?> </li> 
+                 
+             </ul>
+                
+                <?php endfor; ?>    
+            <?php endfor; ?>    
+        </ul>
+
+        
+    <h2>POSTS DESC</h2>
+    <ul>
+        </ul>
+    </body>
 </html>
+
+
+
